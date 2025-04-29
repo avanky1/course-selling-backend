e4cbb25bc2813f1a879dd67119a26c56f56e2fe5
Avanish Yadav <saver2024only@gmail.com>
2025-04-29 18:35:04 +0530

    mongo

--- routes/admin.js
+++ routes/admin.js
@@ -2,26 +2,68 @@ const { Router } = require('express');
 const adminRouter = Router();
 const {adminModel} = require("../db");
 
+const jwt = require("jsonwebtoken");
+const JWT_ADMIN_SECRET = process.env.JWT_ADMIN_SECRET
 
-adminRouter.post("./signup", (req,res) =>{
-     res.send({
-          message: "Admin signup endpoint"
-     });
+adminRouter.post("/signup", async (req,res) =>{
+     const { email, password, firstname, lastname } = req.body;
+ 
+     try {
+       await adminModel.create({
+         email,
+         password,
+         firstname,
+         lastname
+       });
+       res.send({
+         message: "admin signup successful",
+       });
+     } catch (error) {
+       console.error("Signup error:", error);
+       res.status(500).send({  
+         message: "Error during user signup",
+         error: error.message,
+       });
+     }
 })
 
-adminRouter.post("./signin", (req,res) =>{
-     res.send({
-          message: "Admin signin endpoint"
-     });
+adminRouter.post("/signin", async (req,res) =>{
+     const { email, password } = req.body;
+
+     const admin = await adminModel.findOne({
+    email: email,
+    password: password
+    //bcrypt logic here
+  });
+  if(admin){
+    const token = jwt.sign({
+      id: admin._id,
+    }, JWT_ADMIN_SECRET);
+
+    //cookies logic here write
+
+    res.json({
+      token: token,
+    })
+
+  }else{
+    res.status(403).json({
+      message: "Invalid credentials",
+    });
+  }
+
+  res.send({ 
+     message: "admin signin successfully", 
+});
 }) 
 
-adminRouter.post("./", (req,res) =>{
+adminRouter.post("/", (req,res) =>{
      res.send({
           message: "Admin course endpoint"
      }); 
 })
 
-adminRouter.put("./", (req,res) =>{
+adminRouter.put("/", (req,res) =>{
      res.send({
           message: "Admin course adding endpoint"
      });
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const { userModel } = require("../db"); 
 const userRouter = Router();
+const jwt = require('jsonwebtoken');
+const JWT_USER_SECRET = process.env.JWT_USER_SECRET
 
 userRouter.post('/signup', async (req, res) => {
   const { email, password, firstname, lastname } = req.body;
@@ -24,15 +26,40 @@ userRouter.post('/signup', async (req, res) => {
   }
 });
 
-userRouter.post('/signin', (req, res) => {
+
+userRouter.post('/signin', async (req, res) => {
   const { email, password } = req.body;
+
+  const user = await userModel.findOne({
+    email: email,
+    password: password
+    //bcrypt logic here
+  });
+  if(user){
+    const token = jwt.sign({
+      id: user._id,
+    }, JWT_USER_SECRET);
+
+    //cookies logic here write
+
+    res.json({
+      token: token,
+    })
+
+  }else{
+    res.status(403).json({
+      message: "Invalid credentials",
+    });
+  }
+
   res.send({ 
      message: "User signin successfully", 
 });
 });
 
+
 userRouter.get('/purchases', (req, res) => {
-  
+
   res.send({
      message: "User purchase endpoint"
 });
