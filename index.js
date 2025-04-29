13114089be1c9232c213de41a4dea572ddfd7245
Avanish Yadav <saver2024only@gmail.com>
2025-04-29 08:22:14 +0530

    chnages 1

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
-import express from 'express';
-console.log('hello world');
+const express = require("express");
+const app = express();
+
+
+app.post('/user/signup', function(req, res) {
+     res.send({
+            message: "User signed up successfully"
+     });
+
+}) 
+
+app.post('/user/signin', function(req, res) {
+     res.send({
+            message: "User signed in successfully"
+     });
+
+})
+
+app.get('user/purchases', function(req, res) {
+     res.send({
+            message: "users course purchases endpoint"
+     });
+})
+app.get('course/purchase', function(req, res) {
+     res.send({
+            message: "user course purchase"
+     });
+})
+
+
+app.get('/courses', function(req, res) {
+     res.send({
+            message: "all courses endpoint"
+     });
+
+})
+
+
+
+
+
+
+app.listen(300);
